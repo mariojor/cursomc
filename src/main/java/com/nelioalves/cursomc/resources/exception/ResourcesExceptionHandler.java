46fb5be0dc580ef0ecd7836f955abb750c61da0d
Albuquerque <m.albuquerque@accenture.com>
2020-02-11 07:59:54 -0300

    Deletando uma categoria

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourcesExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourcesExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.services.exception.DataIntegrityException;
 import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +17,11 @@ public class ResourcesExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException erro, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), erro.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> objectNotFound(DataIntegrityException erro, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), erro.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
