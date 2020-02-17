3c824cf7f39d670a1eacf0ac16abef18ec389489
Albuquerque <m.albuquerque@accenture.com>
2020-02-17 12:18:17 -0300

    Adicionando senha a cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -16,30 +16,33 @@ public class ClienteNewDTO implements Serializable {
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	@Length(min = 5, max = 120, message = "O tamanho deve entre 5 e 120 caracteres!")
 	private String nome;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	@Email(message = "Email inválido!")
 	private String email;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String cpfOuCnpj;
 	private Integer tipo;
 
 	@NotEmpty(message = "Preenchimento obrigatorio!")
+	private String senha;
+
+	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String logradouro;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String numero;
-	
+
 	private String complemento;
 	private String bairro;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String cep;
 
 	@NotEmpty(message = "Preenchimento obrigatorio!")
 	private String telefone1;
-	
+
 	private String telefone2;
 	private String telefone3;
 
@@ -150,6 +153,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
