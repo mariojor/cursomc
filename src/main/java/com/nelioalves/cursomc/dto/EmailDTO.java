58345afe9cc996aab53875df3872dda9f0326e89
Albuquerque <m.albuquerque@accenture.com>
2020-02-19 16:53:46 -0300

    esqueci minha senha

---
+++ src/main/java/com/nelioalves/cursomc/dto/EmailDTO.java
@@ -0,0 +1,18 @@
+package com.nelioalves.cursomc.dto;
+
+public class EmailDTO {
+
+	private String email;
+
+	public EmailDTO() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
