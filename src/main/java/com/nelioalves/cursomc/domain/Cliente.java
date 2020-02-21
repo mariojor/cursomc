37dcf66457da03364560bef9da4b4418452548ea
Albuquerque <m.albuquerque@accenture.com>
2020-02-21 11:21:25 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -40,8 +40,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	private String imageUrl;
-
 	@JsonIgnore
 	private String senha;
 
@@ -153,14 +151,6 @@ public class Cliente implements Serializable {
 
 	public void addPerfil(Perfil perfil) {
 		perfis.add(perfil.getCod());
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 	@Override
